0e51ba06848b963f8930a19619732b98d48d15f8
Andres Marquez <andresmr955@gmail.com>
2025-05-05 20:34:59 -0400

    creating a hostal page

---
+++ CSS/class38/hotelpractice/main.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i")
+
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open")
+   
+    https://www.youtube.com/watch?v=Z4FpqCDaxrA&ab_channel=WebDesignMastery
+   
+   
+})
